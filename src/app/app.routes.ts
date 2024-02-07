40718ca1bd346ffb52f00f1c2c391280ee60e4b4
Satria Utama <satria.u.s@gmail.com>
2024-02-07 17:11:13 +0100

    update component and route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/container', pathMatch: 'full' },
+  {
+    path: 'container',
+    loadComponent: () =>
+      import('./container/container.component').then(
+        (mod) => mod.ContainerComponent
+      ),
+  },
+];
