0fadd409f213bb211836d27a0fdc498bf5246d36
Satria Utama <satria.u.s@gmail.com>
2024-02-15 15:04:42 +0100

    fix signal unit test

--- src/app/service/data.service.spec.ts
+++ src/app/service/data.service.spec.ts
@@ -1,6 +1,5 @@
 import { TestBed } from '@angular/core/testing';
 
-import { Data } from '../data.model';
 import { DataService } from './data.service';
 
 describe('DataService', () => {
@@ -17,37 +16,25 @@ describe('DataService', () => {
 
   describe('storeValue', () => {
     it('should add data to storedData', () => {
-      let storedData: Data[] = [];
       const data = { date: new Date(), amount: 100 };
       service.storeValue(data);
 
-      const subscription = service.storedData$.subscribe((data) => {
-        storedData = data;
-      });
-      subscription.unsubscribe();
-
-      expect(storedData).toContain(data);
+      expect(service.storedData()).toContain(data);
 
       /* If the parsing is successful and the resulting date object's NaN property is false,
        then the string can be considered valid ISO 8601 */
-      const isIso8601 = !isNaN(Date.parse(storedData[0].date.toISOString()));
+      const isIso8601 = !isNaN(
+        Date.parse(service.storedData()[0].date.toISOString())
+      );
       expect(isIso8601).toBeTrue;
     });
   });
 
   describe('clearValues', () => {
     it('should clear storedData', () => {
-      let storedData: Data[] = [];
-
-      service['storedData'] = [{ date: new Date(), amount: 100 }];
+      service.storeValue({ date: new Date(), amount: 100 });
       service.clearValues();
-
-      const subscription = service.storedData$.subscribe((data) => {
-        storedData = data;
-      });
-      subscription.unsubscribe();
-
-      expect(storedData).toEqual([]);
+      expect(service.storedData()).toEqual([]);
     });
   });
 });
